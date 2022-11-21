2550313c658c8ff574f27aa407aade97145e7a4c
rominirani <romin.k.irani@gmail.com>
2022-11-21 09:49:20 +0000

    modified main.py with new welcome message

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+    return f"Hello {name}. Welcome to Cloud Build!"
 
 
 if __name__ == "__main__":
