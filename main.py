ec0a0d328bea5384196e1c5a2b01d8d26d1ef391
rominirani <romin.k.irani@gmail.com>
2022-11-29 04:05:26 +0000

    updated message

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "Cloud")
+    name = os.environ.get("NAME", "Cloud Build")
     return f"Hello {name}!"
 
 
