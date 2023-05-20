52c5deef6fb3a49951283a83d50b798153e6a46d
Romin Irani <romin.k.irani@gmail.com>
2023-05-20 11:24:21 +0530

    updated the year

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "CCD Indore")
+    name = os.environ.get("NAME", "CCD Indore 2023")
     return f"Hey {name}!"
 
 
